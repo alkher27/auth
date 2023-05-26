23f516fc46ecd30ca5d15c5a6ca2cfaa748a120d
alkher27 <alikheraj@gmail.com>
2023-05-26 16:49:57 +0000

    tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   def index
     if @current_user
-      @tasks = Task.all
+      @tasks = Task.where({"user_id"=>@current_user["id"]})
       @task = Task.new
     end
   end
@@ -10,6 +10,7 @@ class TasksController < ApplicationController
     if @current_user
       @task = Task.new
       @task["description"] = params["task"]["description"]
+      @task["user_id"] = @current_user["id"]
       @task.save
       redirect_to "/tasks"
     end
@@ -18,7 +19,9 @@ class TasksController < ApplicationController
   def destroy
     if @current_user
       @task = Task.find_by({ "id" => params["id"] })
-      @task.destroy
+      if @task["user_id"] == @current_user["id"]
+        @task.destroy
+      end
       redirect_to "/tasks"
     end
   end
